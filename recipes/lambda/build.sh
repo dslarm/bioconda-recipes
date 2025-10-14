0366116f62b93c5372ee267bfda4e0a6315ed0a8
David Lecomber <david+dslarm@lecomber.net>
2025-10-14 16:51:50 +0100

    patches w up to date sharg

--- recipes/lambda/build.sh
+++ recipes/lambda/build.sh
@@ -3,7 +3,12 @@ set -xe
 #if [ $(uname -s) == Darwin -a $(uname -m) != arm64 ]; then
 if [ $(uname -s) == Darwin ]; then
     # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+#    export CXXFLAGS="${CXXFLAGS} -std=c++23"
+#    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+#    wget https://github.com/seqan/sharg-parser/releases/download/1.1.1/sharg-1.1.1-Source.tar.xz
+#    tar xvf sharg-1.1.1-Source.tar.xz
+    #    cp -r sharg-1.1.1-Source/* submodules/sharg-parser
+    cp -r llvm-sharg/* submodules/sharg-parser
 fi
 mkdir -p "$PREFIX/bin"
 mkdir -p build
